441cd0c29a237f14a2c9cf73637891980937b105
Vicente <VicenteJSP@outlook.com>
2019-08-27 12:38:52 -0500

    Agregando columnas y estilo a los textos

--- contador/lib/src/pages/home_page.dart
+++ contador/lib/src/pages/home_page.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+
+  final estiloTexto = TextStyle( fontSize: 25.0);
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Titulo app'),
+        title: Text('Contador'),
         centerTitle: true,
       ),
-      body: Center(child: Text('Hola mundo desde el home Scaffold'),),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text('Número de Clicks:', style: estiloTexto),
+            Text('0', style: estiloTexto),
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ),
     );
   }
 }
